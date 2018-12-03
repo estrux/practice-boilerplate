d95a48a4b49a51a31575db32ae7c1bbbf0080991
Eric Struckhoff <estrux@Erics-MacBook-Air.local>
2018-12-03 00:25:25 -0600

    complete course

---
+++ webpack.config.prod.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generate an external css file with hashed name
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Hash file names for cache-busting
+    new WebpackMd5Hash(),
+    // Commons Chunk for bundle splitting
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    // Create HTML file
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are are available in index.html
+      trackJSToken: 'f522cc585b7242b28a38b3b74c1c73ff'
+    }),
+    // Eliminate duplicate packages
+    new webpack.optimize.DedupePlugin(),
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
